271ea258def7ad157b83dc1a552bcd4d5bbd6207
Konstantinos <kostikas@gmail.com>
2015-10-07 16:10:27 +0300

    Add more files
        * Synergy configuration
        * sysctl minor stuff - maybe outdated
        * hw ilo/idrac/... credentials from various manufacturers - outdated
        * dosage (syntax)
        * Minor stuff

---
+++ scripts/dosage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Dosage is used to download Comics (e.g. xkcd)
+
+if [ ! $(which dosage)  ]; then
+    echo "This script needs package dosage."
+    exit 1
+fi
+
+# Use help to find out more
+dosage --adult -o html -c xkcd
+
+exit 0
