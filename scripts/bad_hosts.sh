4c08041c2fc9785252fec925f40bdc88333f32fc
Konstantinos <kostikas@gmail.com>
2016-02-15 13:55:28 +0200

    Add shell script which aggregates "bad" hostnames
     * The script gathers from various internet locations (lists) malware / adblock
     * list of hostnames and aggregates them into one which can be used for a proxy
     * dns configuration or for a /etc/hosts file.
     *
     * Should be integrated into a cron script to get recent versions.
     *
     * inspired by https://github.com/StevenBlack/hosts
     *
     * Current sources are:
     * https://raw.githubusercontent.com/Free-Software-for-Android/AdAway/master/hosts/hosts.txt
     * https://raw.githubusercontent.com/StevenBlack/hosts/master/data/StevenBlack/hosts
     * http://winhelp2002.mvps.org/hosts.txt
     * http://www.malwaredomainlist.com/hostslist/hosts.txt
     * http://someonewhocares.org/hosts/zero/hosts
     * http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext

---
+++ scripts/bad_hosts.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Version 0.01 - Bad hosts
+# GPL v2
+
+# Script to gather various hostnames from various locations and generate an
+# aggregated list which can be used in bind, squid, /etc/hosts, ... 
+
+# Set a random tmp file
+tmp_file="/tmp/$(date +%s%N | sha256sum | cut -d ' ' -f1).tmp"
+
+dl_lists="
+https://raw.githubusercontent.com/Free-Software-for-Android/AdAway/master/hosts/hosts.txt
+https://raw.githubusercontent.com/StevenBlack/hosts/master/data/StevenBlack/hosts
+http://winhelp2002.mvps.org/hosts.txt
+http://www.malwaredomainlist.com/hostslist/hosts.txt
+http://someonewhocares.org/hosts/zero/hosts
+http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext
+"
+
+for i in $dl_lists ; do 
+    curl -s "$i" >>$tmp_file
+done
+
+# For use in squid (blocked domains) - look for my squid conf/etc
+cat $tmp_file | egrep "^127.0.0.1 |^0.0.0.0 " | sed -e "s/^127.0.0.1 //" -e "s/^0.0.0.0 //" | awk '{print $NF}' | sort | uniq  | wc -l
+
+## For use in /etc/hosts file (either use 127.0.0.1 or 0.0.0.0 - your choice) - I do not recommend it.
+#cat $tmp_file | egrep "^127.0.0.1 |^0.0.0.0 " | sed -e "s/^127.0.0.1 //" -e "s/^0.0.0.0 //" | awk '{print $NF}' | sort | uniq  | sed "s/^/127.0.0.1 /"
+
+# Cleanup
+rm -f $tmp_file
+
+
