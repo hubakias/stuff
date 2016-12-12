c0bb08e0b614be29100ed0e6afc638c4ba3dc039
Konstantinos <kostikas@gmail.com>
2016-12-12 14:55:25 +0200

    Add useful placeholder settings in lsyncd conf

--- etc/lsyncd/lsyncd.conf.lua
+++ etc/lsyncd/lsyncd.conf.lua
@@ -1,12 +1,20 @@
 settings = {
     logfile = "/var/log/lsyncd/lsyncd.log",
+--    pidfile = "/var/run/lsyncd.pid",
+--    nodaemon = "true",
     statusFile = "/var/log/lsyncd/lsyncd.status"
 }
 
 sync {
     default.rsyncssh,
-    source="/mylocaldir/",
-    host="myremote_host",
-    targetdir="/myremotedir",
---    rsyncOpts = {"-e", "/usr/bin/ssh -l myremoteuser -i myremoteuser_priv_key -p my_remote_ssh_port"}
+    source = "/mylocaldir/",
+    host = "myremote_host",
+    targetdir = "/myremotedir",
+--    delete = "true,false,startup,running",
+--    exclude = { '.bak' , '.tmp' },
+--    excludeFrom = "/etc/lsyncd/exclusions",
+--    rsyncOpts = {"-e", "/usr/bin/ssh -l myremoteuser -i myremoteuser_priv_key -p my_remote_ssh_port"},
+    ssh = {
+      port = 1234
+    }
 }
