598b29b79bde7c0a8d1ac0a64a48c0002c67b349
Konstantinos <kostikas@gmail.com>
2017-09-21 16:03:54 +0200

    Improve the git repo update script and extras
     * Update the script which updates all git repos under the current path.
     * Introduce a common script library which at the moments has only
     * declarations for shell colors - if supported.

--- scripts/sync_git_repos.sh
+++ scripts/sync_git_repos.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# Simple script to pull all git repositories under the current path - depth of 1
+# Simple script to pull all git repositories under the current path
 
+# Hide STDERR
+exec 2>/dev/null
+
+# use script library
+. "$(dirname "$0")"/script_lib
+
+# Define variables
 dir_path="$(pwd)"
 
+# Set error messages
+err_dir="${red}Could not change into directory:${normal}"
+err_git="${red}Failed to pull repository!${normal}"
+
+# Execute
 for i in $(find ./ -type d -name '.git' | sed "s/.git$//"); do
-  if [ -d "$dir_path"/"$i"/.git ]; then
-    cd "$dir_path"/"$i"
-    git pull -r
-  fi
+
+  cd "${dir_path}"/"${i}" || { echo "${err_dir} ${i}" ; continue ; }
+  echo -e "\n${green}Pulling:${normal} ${bold}${i}${normal}"
+  git pull --all || { echo "${err_git}" ; continue ; }
+#  git pull --rebase || { echo "${err_git}" ; continue ; }
+
 done
 
+echo
 
+exit 0
