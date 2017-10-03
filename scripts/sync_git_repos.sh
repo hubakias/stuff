3f268fe37c3a7ee962763fdb8079245e3d3b6773
Konstantinos <kostikas@gmail.com>
2017-10-03 18:52:33 +0200

    update git sync script

--- scripts/sync_git_repos.sh
+++ scripts/sync_git_repos.sh
@@ -3,7 +3,7 @@
 # Simple script to pull all git repositories under the current path
 
 # Hide STDERR
-exec 2>/dev/null
+#exec 2>/dev/null
 
 # use script library
 . "$(dirname "$0")"/script_lib
@@ -15,13 +15,50 @@ dir_path="$(pwd)"
 err_dir="${red}Could not change into directory:${normal}"
 err_git="${red}Failed to pull repository!${normal}"
 
+if [ "$1" = "-f" ]; then
+
+  ee="p"
+  eee="yes"
+
+fi
+
+
 # Execute
 for i in $(find ./ -type d -name '.git' | sed "s/.git$//"); do
 
   cd "${dir_path}"/"${i}" || { echo "${err_dir} ${i}" ; continue ; }
-  echo -e "\n${green}Pulling:${normal} ${bold}${i}${normal}"
-  git pull --all || { echo "${err_git}" ; continue ; }
-#  git pull --rebase || { echo "${err_git}" ; continue ; }
+
+  if [ ! "$ee" ]; then read -r -n1 -p "Rebase/Pull (r/p)?" ee ; fi
+
+  if [ "${ee,,}" = "p" ]; then
+
+    echo -e "\n${green}Pulling:${normal} ${bold}${i}${normal}"
+    git pull --all || { echo "${err_git}" ; continue ; }
+
+  elif [ "${ee,,}" = "r" ]; then
+
+    echo -e "\n${green}Rebasing:${normal} ${bold}${i}${normal}"
+    git pull --rebase || { echo "${err_git}" ; continue ; }
+
+  else
+
+    echo "Moving on ..."
+
+  fi
+
+# Cleanup local stale entries
+  if [ ! "$eee" ]; then read -r -n 5 -p "Cleanup ??? (yes/no)" eee ; fi
+
+  if [ "${eee,,}" = "yes" ]; then
+
+    echo -e "\n${green}Cleaning up:${normal} ${bold}${i}${normal}"
+    git remote update --prune origin || { echo "${err_git}" ; continue ; }
+
+  else
+
+    echo -e "\nNothing to do ..."
+
+  fi
 
 done
 
