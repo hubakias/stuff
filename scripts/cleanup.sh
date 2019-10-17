0a566581ad5275ba8a00a96d2fad5cf4c8d1c37e
Konstantinos <kostikas@gmail.com>
2019-10-17 14:19:25 +0200

    Some security hardening and housekeeping

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,9 +3,10 @@
 # Simple script to clean repos, user histories, tmp files and defrag the disks.
 
 yum clean all
-rm -Rf /var/lib/yum/yumdb /var/lib/yum /var/cache/yum/ /tmp/.* /tmp/*
+rm -Rf /var/lib/yum/yumdb /var/lib/yum /var/cache/yum/ /tmp/*
 rm -Rf /home/*/.*history /home/*/.ssh/known_hosts
 rm -Rf /root/.*history /root/.ssh/known_hosts /var/lib/mlocate/mlocate.db
+rm -Rf /root/.cshrc /root/.tcshrc /home/*/.cshrc /home/*/.tcshrc
 rm -Rf /var/log/lost+found /var/log/anaco* # /root/*
 for i in $(find /var/log/ -type f); do echo -n >$i ; done
 
