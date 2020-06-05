28bb722df5124facf3f996eb8412453b7081dce1
Konstantinos <kostikas@gmail.com>
2020-06-05 12:28:52 +0200

    Cleanup sync_repos.sh

--- etc/yum.repos.d/sync_repos.sh
+++ etc/yum.repos.d/sync_repos.sh
@@ -6,8 +6,8 @@
 
 export etcrepo='/etc/yum/repos.d'
 
-local_repo_dir=""
-if [ ! "${local_repo_dir}" ] ; then echo "Set the repository dir"; exit 1 ; fi
+l_dir="/srv/repo" # Variable for the local repository directory
+if [ ! "${l_dir}" ] ; then echo "Set the repository dir"; exit 1 ; fi
 
 etcconf='/etc/yum/yum.conf'
 
@@ -17,10 +17,10 @@ centos8="centos8_base_os centos8_powertools centos8_extras epel8"
 docker="docker_centos7_ce_stable"
 
 for i in ${centos6} ${centos7} ${centos8} ${docker}; do
-      cd "${local_repo_dir}" || (echo "Cannot find repo dir" && exit 1)
+      cd "${l_dir}" || (echo "Cannot find repo dir" && exit 1)
       reposync -l -d -n -c "${etcconf}" -r "$i"
 done | grep -v "Skipping existing Packages"
 
-#mount -o loop rhel-server-6.10-x86_64-dvd.iso rhel6
-#mount -o loop rhel-server-7.7-x86_64-dvd.iso rhel7
-#mount -o loop rhel-8.1-x86_64-dvd.iso rhel8/
+#mount -o loop "${l_dir}"/rhel-server-6.10-x86_64-dvd.iso "${l_dir}"/rhel6
+#mount -o loop "${l_dir}"/rhel-server-7.7-x86_64-dvd.iso "${l_dir}"/rhel7
+#mount -o loop "${l_dir}"/rhel-8.1-x86_64-dvd.iso "${l_dir}"/rhel8/
