61c891ea8af5010011ac2ba2ca89be37f2f794ad
Konstantinos <kostikas@gmail.com>
2023-01-08 07:27:44 +0100

    Improve fir greek subs script

--- scripts/fix_greek_subtitles.sh
+++ scripts/fix_greek_subtitles.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Simple script to convert a .srt file from ISO-8859-7 to UTF-8
+if [ "$(file -b "$1" | awk '{print $1}' | grep UTF-8)" ]; then
+  echo "It seems the file is already UTF-8 encoded. Nothing to do..."
+  exit 1
+fi
 
-if [ ! "$1" ]; then
-  echo "Please provide an srt, ISO-8859 encoded, file."; exit 1
+if [ ! "$(file -b "$1" 2>/dev/null | awk '{print $1}' | grep ISO-8859)" ]; then
+  echo "Please provide an ISO-8859 encoded srt file."
+  exit 1
 fi
 
 if [ ! "${1/*\./}" == "srt" ]; then
   echo "Only srt file extensions are supported."; exit 1
 fi
 
-#if [ ! "$(file -b "$1" | cut -d ' ' -f1)" == "ISO-8859" ]; then
-#  echo "The input file must be ISO-8869 encoded."; exit 1
-#fi
-
 iconv -f ISO-8859-7 -t utf8 "${1}" -o "${1}".fixed
 mv -f "${1}".fixed "${1}"
 sed -i "s/\\’/'Α/" "${1}"
-
+sed -i 's/\r$//' "${1}"
